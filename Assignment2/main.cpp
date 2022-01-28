69d954a8ce3d3c06a6f7306e6973dc4803953b5a
krunalbadlani <84794768+krunalbadlani@users.noreply.github.com>
2022-01-28 08:21:13 +0530

    Update main.cpp

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -1,14 +1,27 @@
 #include <Arduino.h>
-int n=13;
-void setup() {
-  pinMode(n,OUTPUT);
-  // put your setup code here, to run once:
-}
 
-void loop() {
-  digitalWrite(n,HIGH);
-  delay(500);
-  digitalWrite(n,LOW);
-  delay(500);
-  // put your main code here, to run repeatedly:
+int X=2,Y=3,Z=4;
+int output;
+
+void setup()
+{
+pinMode(X, INPUT);
+pinMode(Y, INPUT);
+pinMode(Z, INPUT);
+pinMode(13, OUTPUT);
+
+}
+void loop()
+{int x=digitalRead(X);
+ int y=digitalRead(Y);
+ int z=digitalRead(Z);
+        int a= (!(!x && y ));
+        int b= (!(y && !z));
+        int c= (!(x && !y && z));
+        output=(!(!((!(!(a && b))) && (c))));
+if(output==1)
+digitalWrite(13,HIGH);
+else
+digitalWrite(13,LOW);
+
 }
